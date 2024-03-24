8c60828c25d342a29aa49c3f53561379e9ce99d3
jspark_0926@naver.com <jspark_0926@naver.com>
2024-03-24 21:39:52 +0900

    commit message add

---
+++ lib/new_class.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class newClass extends StatelessWidget {
+  const newClass({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
